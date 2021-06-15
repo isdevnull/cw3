2af961f4379d0e913d6b94f076b0f18155f04a22
Ivan Shchekotov <ivanxshchekotov@gmail.com>
2021-06-15 16:55:09 +0300

    Modified .gitignore to exclude environment folder
    Files with encodings were moved to utils/ folder.
    According changes were made to preprocessing .ipynb notebook.
    
    Now clean_data has 'date' column in it, as well as all other features
    except encodings are in English.

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from .health_status import injury_to_severity_pedestrian, injury_to_severity_driver
+from .transport import transport_to_groups
+from .violations import violation_encoding_driver, violation_encoding_pedestrian
+from .nearby import property2cat
